61b3bba4f6701d65fd94c5c2b480f6ab9a7db354
nb18411 <nbesimi@hotmail.com>
2012-11-22 00:42:11 +0100

    updated post part

--- SocialCee/SocialCee/src/com/example/socialcee/Post.java
+++ SocialCee/SocialCee/src/com/example/socialcee/Post.java
@@ -20,7 +20,7 @@ public class Post extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_post);
         LinearLayout content = new LinearLayout(this);
         content = (LinearLayout) findViewById(R.id.content); 
         LinearLayout ll = CreateBlock(1);
