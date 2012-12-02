54a82b4eee21a58829434f374671a486d15faeba
nb18411 <nbesimi@hotmail.com>
2012-12-02 17:21:35 +0100

    JSON Request Update

--- SocialCee/SocialCee/src/com/example/socialcee/JsonRequest.java
+++ SocialCee/SocialCee/src/com/example/socialcee/JsonRequest.java
@@ -1,13 +1,19 @@
 package com.example.socialcee;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 
 public class JsonRequest {
 
@@ -25,26 +31,25 @@ public class JsonRequest {
 	{
 		url = URL;
 	}
-	public String getJson() 
+	public String getJson(String temp) 
 	{
-		HttpClient httpClient = new DefaultHttpClient();
-		HttpGet httpGet = new HttpGet(url);
-		ResponseHandler<String> responseHandler = new BasicResponseHandler();
-		String response = new String();
-		try 
-		{
-			response = httpClient.execute(httpGet, responseHandler);
-		} 
-		catch (ClientProtocolException e) 
-		{
-			e.printStackTrace();
-		} 
-		catch (IOException e1) 
-		{
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		return response;
+   		String response = new String();
+   		HttpClient httpclient = new DefaultHttpClient();
+	   	HttpPost httppost = new HttpPost("https://api.socialcee.com/services/AccountsHandler.ashx");
+	   	try 
+	   	{
+	   	   List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(1);
+	       nameValuePairs.add(new BasicNameValuePair("apikey", "96decc6c17394f2c984ac8bc42da9a54"));
+	       httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+	   	   httpclient.execute(httppost);
+	   	   ResponseHandler<String> responseHandler = new BasicResponseHandler();
+	   	   response = httpclient.execute(httppost, responseHandler);
+	     } catch (ClientProtocolException e) {
+	         // TODO Auto-generated catch block
+	     } catch (IOException e) {
+	         // TODO Auto-generated catch block
+	     }
+	   	 return response;
 	}
 		
     }
