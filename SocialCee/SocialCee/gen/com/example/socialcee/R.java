d422f4b5f8650d962f7796b92b60c74a4eb1d5dd
Mimozë Lladrovci <mimoza.ll@hotmail.com>
2012-12-05 13:05:40 +0100

    Accounts
    Hardcoded

--- SocialCee/SocialCee/gen/com/example/socialcee/R.java
+++ SocialCee/SocialCee/gen/com/example/socialcee/R.java
@@ -11,30 +11,40 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int accounts=0x7f020000;
-        public static final int arrow=0x7f020001;
-        public static final int comment=0x7f020002;
-        public static final int group=0x7f020003;
-        public static final int ic_action_search=0x7f020004;
-        public static final int ic_launcher=0x7f020005;
-        public static final int internet_connection=0x7f020006;
-        public static final int one=0x7f020007;
-        public static final int plus=0x7f020008;
-        public static final int posts=0x7f020009;
-        public static final int three=0x7f02000a;
-        public static final int two=0x7f02000b;
-        public static final int users=0x7f02000c;
+        public static final int a=0x7f020000;
+        public static final int accounts=0x7f020001;
+        public static final int arrow=0x7f020002;
+        public static final int b=0x7f020003;
+        public static final int c=0x7f020004;
+        public static final int comment=0x7f020005;
+        public static final int d=0x7f020006;
+        public static final int group=0x7f020007;
+        public static final int ic_action_search=0x7f020008;
+        public static final int ic_launcher=0x7f020009;
+        public static final int internet_connection=0x7f02000a;
+        public static final int one=0x7f02000b;
+        public static final int plus=0x7f02000c;
+        public static final int posts=0x7f02000d;
+        public static final int three=0x7f02000e;
+        public static final int two=0x7f02000f;
+        public static final int users=0x7f020010;
     }
     public static final class id {
         public static final int button=0x7f070001;
         public static final int content=0x7f070005;
+        public static final int desc=0x7f07000a;
         public static final int grid_view=0x7f070000;
         public static final int header=0x7f070002;
+        public static final int icon=0x7f070008;
+        public static final int list=0x7f07000d;
         public static final int mainImage=0x7f070006;
         public static final int maincontent=0x7f070003;
-        public static final int menu_settings=0x7f070008;
+        public static final int menu_settings=0x7f07000e;
         public static final int overlayImage=0x7f070007;
         public static final int scrollView1=0x7f070004;
+        public static final int textView1=0x7f07000c;
+        public static final int textView2=0x7f07000b;
+        public static final int title=0x7f070009;
     }
     public static final class layout {
         public static final int activity_accounts=0x7f030000;
@@ -45,6 +55,9 @@ public final class R {
         public static final int activity_selected_grid=0x7f030005;
         public static final int activity_users=0x7f030006;
         public static final int customview=0x7f030007;
+        public static final int list=0x7f030008;
+        public static final int list_item=0x7f030009;
+        public static final int main=0x7f03000a;
     }
     public static final class menu {
         public static final int activity_accounts=0x7f060000;
@@ -60,6 +73,7 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
+        public static final int image=0x7f04000b;
         public static final int menu_settings=0x7f040002;
         public static final int title_activity_accounts=0x7f040007;
         public static final int title_activity_grid_view=0x7f040004;
--- SocialCee/SocialCee/src/com/example/socialcee/Accounts.java
+++ SocialCee/SocialCee/src/com/example/socialcee/Accounts.java
@@ -1,20 +1,42 @@
 package com.example.socialcee;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.widget.ListView;
 
 public class Accounts extends Activity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_accounts);
+	public static final String[] titles = new String[] { "Shqiperia",
+        "Kosova", "Maqedonia", "Mali i Zi" };
+
+public static final String[] descriptions = new String[] {
+        "hhasss",
+        "dbdba", "ndfjfjda",
+        "djadjad" };
+
+public static final Integer[] images = {R.drawable.a,R.drawable.b, R.drawable.c, R.drawable.d};
+
+ListView listView;
+List<RowItem> rowItems;
+
+/** Called when the activity is first created. */
+@Override
+public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+
+    rowItems = new ArrayList<RowItem>();
+    for (int i = 0; i < titles.length; i++) {
+        RowItem item = new RowItem(images[i], titles[i], descriptions[i]);
+        rowItems.add(item);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_accounts, menu);
-        return true;
-    }
+    listView = (ListView) findViewById(R.id.list);
+    CustomListViewAdapter adapter = new CustomListViewAdapter(this,
+            R.layout.list_item, rowItems);
+    listView.setAdapter(adapter);
+}
 }
