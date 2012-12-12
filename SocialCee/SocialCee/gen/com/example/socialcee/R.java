8d513c7ceb4fd2e54cac5aa46b18e8aeb5cbef69
nb18411 <nbesimi@hotmail.com>
2012-12-12 23:42:51 +0100

    Mimoza's Account Part

--- SocialCee/SocialCee/gen/com/example/socialcee/R.java
+++ SocialCee/SocialCee/gen/com/example/socialcee/R.java
@@ -23,13 +23,14 @@ public final class R {
         public static final int ic_launcher=0x7f020009;
         public static final int internet_connection=0x7f02000a;
         public static final int logo=0x7f02000b;
-        public static final int newposts=0x7f02000c;
-        public static final int one=0x7f02000d;
-        public static final int plus=0x7f02000e;
-        public static final int posts=0x7f02000f;
-        public static final int three=0x7f020010;
-        public static final int two=0x7f020011;
-        public static final int users=0x7f020012;
+        public static final int maks_norway=0x7f02000c;
+        public static final int newposts=0x7f02000d;
+        public static final int one=0x7f02000e;
+        public static final int plus=0x7f02000f;
+        public static final int posts=0x7f020010;
+        public static final int three=0x7f020011;
+        public static final int two=0x7f020012;
+        public static final int users=0x7f020013;
     }
     public static final class id {
         public static final int btnLogin=0x7f070004;
@@ -38,14 +39,15 @@ public final class R {
         public static final int grid_view=0x7f070000;
         public static final int header=0x7f070001;
         public static final int icon=0x7f070008;
-        public static final int list=0x7f07000d;
+        public static final int list=0x7f07000c;
         public static final int mainImage=0x7f070006;
         public static final int maincontent=0x7f070002;
-        public static final int menu_settings=0x7f07000e;
+        public static final int menu_settings=0x7f07000f;
         public static final int overlayImage=0x7f070007;
         public static final int scrollView1=0x7f070003;
-        public static final int textView1=0x7f07000c;
-        public static final int textView2=0x7f07000b;
+        public static final int textView1=0x7f07000b;
+        public static final int textView2=0x7f07000d;
+        public static final int textView3=0x7f07000e;
         public static final int title=0x7f070009;
     }
     public static final class layout {
@@ -75,6 +77,8 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int email=0x7f04000d;
+        public static final int facebook=0x7f040010;
+        public static final int google=0x7f040011;
         public static final int hello_world=0x7f040001;
         public static final int image=0x7f04000b;
         public static final int login=0x7f04000e;
@@ -88,6 +92,7 @@ public final class R {
         public static final int title_activity_posts=0x7f04000a;
         public static final int title_activity_selected_grid=0x7f040005;
         public static final int title_activity_users=0x7f040008;
+        public static final int twitter=0x7f04000f;
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;
--- SocialCee/SocialCee/src/com/example/socialcee/Accounts.java
+++ SocialCee/SocialCee/src/com/example/socialcee/Accounts.java
@@ -9,34 +9,33 @@ import android.widget.ListView;
 
 public class Accounts extends Activity {
 
-	public static final String[] titles = new String[] { "Shqiperia",
-        "Kosova", "Maqedonia", "Mali i Zi" };
+	public static final String[] titles = new String[] { "First Page",
+			"Second Page", "Third Page", "Fourth page" };
 
-public static final String[] descriptions = new String[] {
-        "hhasss",
-        "dbdba", "ndfjfjda",
-        "djadjad" };
+	public static final String[] descriptions = new String[] { "description1",
+			"description2", "description3", "description4" };
 
-public static final Integer[] images = {R.drawable.a,R.drawable.b, R.drawable.c, R.drawable.d};
+	public static final Integer[] images = { R.drawable.internet_connection,
+			R.drawable.maks_norway, R.drawable.c, R.drawable.d };
 
-ListView listView;
-List<RowItem> rowItems;
+	ListView listView;
+	List<RowItem> rowItems;
 
-/** Called when the activity is first created. */
-@Override
-public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.main);
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
 
-    rowItems = new ArrayList<RowItem>();
-    for (int i = 0; i < titles.length; i++) {
-        RowItem item = new RowItem(images[i], titles[i], descriptions[i]);
-        rowItems.add(item);
-    }
+		rowItems = new ArrayList<RowItem>();
+		for (int i = 0; i < titles.length; i++) {
+			RowItem item = new RowItem(images[i], titles[i], descriptions[i]);
+			rowItems.add(item);
+		}
 
-    listView = (ListView) findViewById(R.id.list);
-    CustomListViewAdapter adapter = new CustomListViewAdapter(this,
-            R.layout.list_item, rowItems);
-    listView.setAdapter(adapter);
-}
+		listView = (ListView) findViewById(R.id.list);
+		CustomListViewAdapter adapter = new CustomListViewAdapter(this,
+				R.layout.list_item, rowItems);
+		listView.setAdapter(adapter);
+	}
 }
